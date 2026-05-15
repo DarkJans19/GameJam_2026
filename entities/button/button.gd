fed4bdf4da59bad918275783bb6d68a6b54c6f7d
DarkJans19 <jansito4221@gmail.com>
2026-05-15 09:28:39 -0500

    Organized correctly in folders, but is probable that the paths be incorrect

---
+++ entities/button/button.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+signal hovered
+signal hovered_off
+signal selected
+signal description
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# In this case all buttons must be childs of maybe player or turn_manager otherwise will fail
+	get_parent().connect_button_signals(self)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_mouse_entered() -> void:
+	emit_signal("hovered", self)
+	
+	
+func _on_area_2d_mouse_exited() -> void:
+	emit_signal("hovered_off", self)
+
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		emit_signal("selected", self)
+
