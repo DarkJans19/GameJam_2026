b8fe8b6e1c9b6e983586f57908e72e68398ae865
JSRodriguez16 <ch.sebastian16@hotmail.com>
2026-05-14 21:41:42 -0500

    map and config functionality

--- stages/map/map.gd
+++ stages/map/map.gd
@@ -1,6 +1,29 @@
 extends Control
 class_name Map
 
+const PLAYER_HEIGHT_OFFSET := 25
+
+const ENEMY_SCENES := [
+	"res://stages/combat/combat_1.tscn",
+	"res://stages/combat/combat_2.tscn",
+	"res://stages/combat/combat_3.tscn",
+]
+
+const SHOP_SCENES := [
+	"res://stages/map/shop_1.tscn",
+	"res://stages/map/shop_2.tscn",
+]
+
+const RANDOM_SCENES := [
+	"res://stages/map/random_1.tscn",
+	"res://stages/map/random_2.tscn",
+	"res://stages/map/random_3.tscn",
+]
+
+const BOSS_SCENES := [
+	"res://stages/map/boss_1.tscn",
+]
+
 const EVENT_STEPS: Array = [
 	["Enemy_event"],
 	["Enemy_event2"],
@@ -13,100 +36,247 @@ const EVENT_STEPS: Array = [
 	["Boss_event"],
 ]
 
-const EVENT_SCENES := {
-	"Enemy_event": "res://stages/combat/combat.tscn",
-	"Enemy_event2": "res://stages/combat/combat.tscn",
-	"Enemy_event3": "res://stages/combat/combat.tscn",
-	"Enemy_event4": "res://stages/combat/combat.tscn",
-	"Enemy_event5": "res://stages/combat/combat.tscn",
-	"Enemy_event6": "res://stages/combat/combat.tscn",
-	"Random_event": "res://stages/map/random_event.tscn",
-	"Random_event2": "res://stages/map/random_event.tscn",
-	"Random_event3": "res://stages/map/random_event.tscn",
-	"Random_event4": "res://stages/map/random_event.tscn",
-	"Random_event5": "res://stages/map/random_event.tscn",
-	"Random_event6": "res://stages/map/random_event.tscn",
-	"Shop_event": "res://stages/map/shop_event.tscn",
-	"Shop_event2": "res://stages/map/shop_event.tscn",
-	"Boss_event": "res://stages/map/boss_event.tscn",
-}
-
 static var current_event: int = 0
+static var last_selected_event: String = ""
 
 @onready var player: TextureRect = $Player
+
 @onready var event_nodes: Dictionary = {
 	"Enemy_event": $Enemy_event,
-	"Random_event": $Random_event,
-	"Shop_event": $Shop_event,
-	"Boss_event": $Boss_event,
 	"Enemy_event2": $Enemy_event2,
-	"Random_event2": $Random_event2,
-	"Random_event3": $Random_event3,
 	"Enemy_event3": $Enemy_event3,
 	"Enemy_event4": $Enemy_event4,
 	"Enemy_event5": $Enemy_event5,
 	"Enemy_event6": $Enemy_event6,
-	"Shop_event2": $Shop_event2,
+
+	"Random_event": $Random_event,
+	"Random_event2": $Random_event2,
+	"Random_event3": $Random_event3,
 	"Random_event4": $Random_event4,
 	"Random_event5": $Random_event5,
 	"Random_event6": $Random_event6,
+
+	"Shop_event": $Shop_event,
+	"Shop_event2": $Shop_event2,
+
+	"Boss_event": $Boss_event,
 }
 
 static func reset_progress() -> void:
 	current_event = 0
+	last_selected_event = ""
 
 func _ready() -> void:
+
+	randomize()
+
+	player.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	player.z_index = 100
+
 	_connect_events()
 	_refresh_map()
 
 func _connect_events() -> void:
+
 	for event_name in event_nodes.keys():
+
 		var event_node: Control = event_nodes[event_name]
-		event_node.gui_input.connect(_on_event_gui_input.bind(event_name))
+
+		if not event_node.gui_input.is_connected(
+			_on_event_gui_input
+		):
+
+			event_node.gui_input.connect(
+				_on_event_gui_input.bind(event_name)
+			)
 
 func _refresh_map() -> void:
+
 	for step_index in range(EVENT_STEPS.size()):
+
+		var enabled := (
+			step_index == current_event
+		)
+
 		for event_name in EVENT_STEPS[step_index]:
-			_set_event_enabled(event_nodes[event_name], step_index == current_event)
 
-	if current_event < EVENT_STEPS.size():
-		var active_event_name: String = EVENT_STEPS[current_event][0]
-		player.global_position = event_nodes[active_event_name].global_position
+			_set_event_enabled(
+				event_nodes[event_name],
+				enabled
+			)
 
-func _on_event_gui_input(event: InputEvent, event_name: String) -> void:
-	# temporary debug prints to verify clicks reach the map
+	if last_selected_event == "":
+
+		if current_event < EVENT_STEPS.size():
+
+			var first_event: String = (
+				EVENT_STEPS[current_event][0]
+			)
+
+			_position_player_initial(
+				event_nodes[first_event]
+			)
+
+	else:
+
+		_position_player_completed(
+			event_nodes[last_selected_event]
+		)
+
+func _position_player_initial(
+	target_node: Control
+) -> void:
+
+	var target_center := (
+		target_node.global_position +
+		(target_node.size * 0.5)
+	)
+
+	var player_size := player.size
+
+	player.global_position = Vector2(
+		target_center.x - (player_size.x * 0.25),
+		target_node.global_position.y - player_size.y + 15
+	)
+
+func _position_player_completed(
+	target_node: Control
+) -> void:
+
+	var target_center := (
+		target_node.global_position +
+		(target_node.size * 0.5)
+	)
+
+	var player_size := player.size
+
+	player.global_position = Vector2(
+		target_center.x - (player_size.x * 0.25),
+		target_node.global_position.y
+	)
+
+func _on_event_gui_input(
+	event: InputEvent,
+	event_name: String
+) -> void:
+
 	if not _is_event_selectable(event_name):
-		print("gui_input ignored - not selectable:", event_name)
 		return
 
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("gui_input clicked:", event_name)
+
+		if (
+			event.button_index
+			== MOUSE_BUTTON_LEFT
+			and event.pressed
+		):
+
+			accept_event()
+
 			_select_event(event_name)
 
-func _is_event_selectable(event_name: String) -> bool:
+func _is_event_selectable(
+	event_name: String
+) -> bool:
+
 	if current_event >= EVENT_STEPS.size():
 		return false
 
-	return event_name in EVENT_STEPS[current_event]
+	return (
+		event_name
+		in EVENT_STEPS[current_event]
+	)
 
-func _select_event(event_name: String) -> void:
-	var scene_path: String = EVENT_SCENES.get(event_name, "")
+func _select_event(
+	event_name: String
+) -> void:
+
+	var scene_path: String = ""
+
+	if event_name.begins_with(
+		"Enemy_event"
+	):
+
+		scene_path = (
+			ENEMY_SCENES.pick_random()
+		)
+
+	elif event_name.begins_with(
+		"Shop_event"
+	):
+
+		scene_path = (
+			SHOP_SCENES.pick_random()
+		)
+
+	elif event_name.begins_with(
+		"Random_event"
+	):
+
+		scene_path = (
+			RANDOM_SCENES.pick_random()
+		)
+
+	elif event_name.begins_with(
+		"Boss_event"
+	):
+
+		scene_path = (
+			BOSS_SCENES.pick_random()
+		)
+
 	if scene_path == "":
+
+		push_error(
+			"No scene assigned to: "
+			+ event_name
+		)
+
 		return
 
-	player.global_position = event_nodes[event_name].global_position
+	last_selected_event = event_name
+
 	current_event += 1
+
+	if event_name.begins_with(
+		"Boss_event"
+	):
+
+		reset_progress()
+
+		get_tree().change_scene_to_file(
+			"res://stages/menu/menu.tscn"
+		)
+
+		return
+
 	_refresh_map()
-	get_tree().change_scene_to_file(scene_path)
 
-func _set_event_enabled(event_node: Control, enabled: bool) -> void:
-	_set_control_mouse_filter(event_node, Control.MOUSE_FILTER_STOP if enabled else Control.MOUSE_FILTER_IGNORE)
-	event_node.modulate = Color(1, 1, 1, 1) if enabled else Color(1, 1, 1, 0.35)
+	get_tree().change_scene_to_file(
+		scene_path
+	)
 
-func _set_control_mouse_filter(node: Node, filter_value: Control.MouseFilter) -> void:
-	if node is Control:
-		(node as Control).mouse_filter = filter_value
+func _set_event_enabled(
+	event_node: Control,
+	enabled: bool
+) -> void:
 
-	for child in node.get_children():
-		_set_control_mouse_filter(child, filter_value)
+	event_node.mouse_filter = (
+		Control.MOUSE_FILTER_STOP
+		if enabled
+		else Control.MOUSE_FILTER_IGNORE
+	)
+
+	for child in event_node.get_children():
+
+		if child is Control:
+
+			child.mouse_filter = (
+				Control.MOUSE_FILTER_IGNORE
+			)
+
+	event_node.modulate = (
+		Color(1, 1, 1, 1)
+		if enabled
+		else Color(1, 1, 1, 0.35)
+	)
--- stages/options/config.gd
+++ stages/options/config.gd
@@ -1,11 +1,213 @@
-extends Node
+extends Control
 
+const CONFIG_PATH := "user://settings.cfg"
 
-# Called when the node enters the scene tree for the first time.
+@onready var master_volume: HSlider = (
+	$VBoxContainer/HBoxContainer/MasterVolume
+)
+
+@onready var music_volume: HSlider = (
+	$VBoxContainer/HBoxContainer2/MusicVolume
+)
+
+@onready var sfx_volume: HSlider = (
+	$VBoxContainer/HBoxContainer3/SFXVolume
+)
+
+@onready var fullscreen_toggle: CheckButton = (
+	$VBoxContainer/HBoxContainer4/FullScreen
+)
+
+@onready var resolution_selector: OptionButton = (
+	$VBoxContainer/HBoxContainer5/Resolution
+)
+
+@onready var back_button: Button = (
+	$VBoxContainer/HBoxContainer6/BackButton
+)
+
+var config := ConfigFile.new()
+
 func _ready() -> void:
-	pass # Replace with function body.
 
+	_setup_resolutions()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	_load_settings()
+
+	master_volume.value_changed.connect(
+		_on_master_changed
+	)
+
+	music_volume.value_changed.connect(
+		_on_music_changed
+	)
+
+	sfx_volume.value_changed.connect(
+		_on_sfx_changed
+	)
+
+	fullscreen_toggle.toggled.connect(
+		_on_fullscreen_toggled
+	)
+
+	resolution_selector.item_selected.connect(
+		_on_resolution_selected
+	)
+
+	back_button.pressed.connect(
+		_on_back_pressed
+	)
+
+func _setup_resolutions() -> void:
+
+	resolution_selector.clear()
+
+	resolution_selector.add_item("1280x720")
+	resolution_selector.add_item("1600x900")
+	resolution_selector.add_item("1920x1080")
+
+func _on_master_changed(value: float) -> void:
+
+	var bus := AudioServer.get_bus_index("Master")
+
+	AudioServer.set_bus_volume_db(
+		bus,
+		linear_to_db(value / 100.0)
+	)
+
+	_save_settings()
+
+func _on_music_changed(value: float) -> void:
+
+	var bus := AudioServer.get_bus_index("Music")
+
+	AudioServer.set_bus_volume_db(
+		bus,
+		linear_to_db(value / 100.0)
+	)
+
+	_save_settings()
+
+func _on_sfx_changed(value: float) -> void:
+
+	var bus := AudioServer.get_bus_index("SFX")
+
+	AudioServer.set_bus_volume_db(
+		bus,
+		linear_to_db(value / 100.0)
+	)
+
+	_save_settings()
+
+func _on_fullscreen_toggled(enabled: bool) -> void:
+
+	if enabled:
+
+		DisplayServer.window_set_mode(
+			DisplayServer.WINDOW_MODE_FULLSCREEN
+		)
+
+	else:
+
+		DisplayServer.window_set_mode(
+			DisplayServer.WINDOW_MODE_WINDOWED
+		)
+
+	_save_settings()
+
+func _on_resolution_selected(index: int) -> void:
+
+	var resolutions := [
+		Vector2i(1280, 720),
+		Vector2i(1600, 900),
+		Vector2i(1920, 1080),
+	]
+
+	DisplayServer.window_set_size(
+		resolutions[index]
+	)
+
+	_save_settings()
+
+func _on_back_pressed() -> void:
+
+	get_tree().change_scene_to_file(
+		"res://stages/menu/menu.tscn"
+	)
+
+func _save_settings() -> void:
+
+	config.set_value(
+		"audio",
+		"master",
+		master_volume.value
+	)
+
+	config.set_value(
+		"audio",
+		"music",
+		music_volume.value
+	)
+
+	config.set_value(
+		"audio",
+		"sfx",
+		sfx_volume.value
+	)
+
+	config.set_value(
+		"video",
+		"fullscreen",
+		fullscreen_toggle.button_pressed
+	)
+
+	config.set_value(
+		"video",
+		"resolution",
+		resolution_selector.selected
+	)
+
+	config.save(CONFIG_PATH)
+
+func _load_settings() -> void:
+
+	if config.load(CONFIG_PATH) != OK:
+		return
+
+	master_volume.value = config.get_value(
+		"audio",
+		"master",
+		100.0
+	)
+
+	music_volume.value = config.get_value(
+		"audio",
+		"music",
+		100.0
+	)
+
+	sfx_volume.value = config.get_value(
+		"audio",
+		"sfx",
+		100.0
+	)
+
+	fullscreen_toggle.button_pressed = config.get_value(
+		"video",
+		"fullscreen",
+		false
+	)
+
+	resolution_selector.selected = config.get_value(
+		"video",
+		"resolution",
+		0
+	)
+
+	_on_resolution_selected(
+		resolution_selector.selected
+	)
+
+	_on_fullscreen_toggled(
+		fullscreen_toggle.button_pressed
+	)
